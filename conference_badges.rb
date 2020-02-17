c8a2a937b0ada7ef8fe6cbfdbd2d939d4218fcd8
Ryan Hollins <ballbaroi@gmail.com>
2020-02-17 14:12:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,4 +20,10 @@ def assign_rooms(speakers)
   room_messages
 end
 def printer(attendees)
-  batch
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
